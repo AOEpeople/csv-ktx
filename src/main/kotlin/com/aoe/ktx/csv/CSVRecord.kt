758889c480662fe245a90dbfbed50aaf777fae2b
Daniel Kopp <daniel.kopp@aoe.com>
2018-06-08 10:27:35 +0200

    Add two new Extension for CSV Records

--- src/main/kotlin/com/aoe/ktx/csv/CSVRecord.kt
+++ src/main/kotlin/com/aoe/ktx/csv/CSVRecord.kt
@@ -19,9 +19,7 @@ package com.aoe.ktx.csv
 import org.apache.commons.csv.CSVRecord
 
 /**
- * Returns a value by name, if not mapped returns the given default value
- *
- * @return the value found or default if not found
+ * Returns a column value by [name] if mapped  else the given [default] value
  */
 operator fun CSVRecord.get(name: String, default: String): String = when {
     isMapped(name) -> this[name]
@@ -29,7 +27,25 @@ operator fun CSVRecord.get(name: String, default: String): String = when {
 }
 
 /**
- * Check if there is any value in given fields in the record
+ * Returns `true` if any value in given [columnName] is not empty.
+ * If no [columnName] given it will search in the whole record
  */
-fun CSVRecord.anyNotEmptyValue(vararg field: String) =
-        field.any { isMapped(it) && this[it].isNotEmpty() }
+fun CSVRecord.anyNotEmptyValue(vararg columnName: String) = when {
+    columnName.isEmpty() -> any { it.isNotEmpty() }
+    else -> columnName.any { isMapped(it) && this[it].isNotEmpty() }
+}
+
+/**
+ * Returns `true` if any column value in [columnName] equals the [expected] String.
+ * If no [columnName] given it will search in the whole record
+ */
+fun CSVRecord.anyValue(expected: String, vararg columnName: String) = when {
+    columnName.isEmpty() -> any { it == expected }
+    else -> columnName.any { isMapped(it) && this[it] == expected }
+}
+
+/**
+ * Returns `true` if expected of [column] given in [expected]
+ */
+fun CSVRecord.valueContains(column: String, vararg expected: String) =
+    isMapped(column) && expected.contains(this[column])
